bedc66832912e1b49eb8835a64d9c037b51ec23c
mohamed-soubhi <Eng.mohamed.soubhi@gmail.com>
2023-01-07 21:02:26 +0100

    telegram code

--- telegrambot.py
+++ telegrambot.py
@@ -1,193 +1,84 @@
-import csv
-import json
-import requests
-import pandas as pd
-import os
-import sys
-import time
-import telepot
+import logging 
+# which allows you to log messages from your application
 
-import warnings
-warnings.filterwarnings("ignore")
+from telegram import Update 
+# An Update represents an incoming update from the Telegram API
+from telegram import InlineQueryResultArticle 
+# This class represents a single result of an inline query
+from telegram import InputTextMessageContent 
+# This class represents the content of a text message to be sent as the result of an inline query
+from telegram.ext import filters 
+# This module provides various filters that can be used to determine if a update should be passed to a handler
+from telegram.ext import MessageHandler 
+# This class is used to handle updates that contain messages
+from telegram.ext import ApplicationBuilder 
+# This class is used to create and configure a Telegram bot
+from telegram.ext import CommandHandler
+# The CommandHandler class is used to handle updates that contain commands
+from telegram.ext import ContextTypes 
+#  the ContextTypes object contains constants that represent the different types of context that a command can be run in
+from telegram.ext import InlineQueryHandler 
+# This class is used to handle updates that contain inline queries
 
-f = open("/home/msa/TelegramBot/bottoken.txt", 'r')
-bottoken = f.readline()
-f.close()
-#bottoken = '5296769845:AAHSNpG_pPfECfa_wTXuot4FUhERxlfQDlU'
-print(bottoken)
 
-# Global variables
- #list
-Surah_ayah_max = [
-                    [ 1 , 7 ],
-                    [ 2 , 286 ],
-                    [ 3 , 200 ],
-                    [ 4 , 176 ],
-                    [ 5 , 120 ],
-                    [ 6 , 165 ],
-                    [ 7 , 206 ],
-                    [ 8 , 75 ],
-                    [ 9 , 129 ],
-                    [ 10 , 109 ],
-                    [ 11 , 123 ],
-                    [ 12 , 111 ],
-                    [ 13 , 43 ],
-                    [ 14 , 52 ],
-                    [ 15 , 99 ],
-                    [ 16 , 128 ],
-                    [ 17 , 111 ],
-                    [ 18 , 110 ],
-                    [ 19 , 98 ],
-                    [ 20 , 135 ],
-                    [ 21 , 112 ],
-                    [ 22 , 78 ],
-                    [ 23 , 118 ],
-                    [ 24 , 64 ],
-                    [ 25 , 77 ],
-                    [ 26 , 227 ],
-                    [ 27 , 93 ],
-                    [ 28 , 88 ],
-                    [ 29 , 69 ],
-                    [ 30 , 60 ],
-                    [ 31 , 34 ],
-                    [ 32 , 30 ],
-                    [ 33 , 73 ],
-                    [ 34 , 54 ],
-                    [ 35 , 45 ],
-                    [ 36 , 83 ],
-                    [ 37 , 182 ],
-                    [ 38 , 88 ],
-                    [ 39 , 75 ],
-                    [ 40 , 85 ],
-                    [ 41 , 54 ],
-                    [ 42 , 53 ],
-                    [ 43 , 89 ],
-                    [ 44 , 59 ],
-                    [ 45 , 37 ],
-                    [ 46 , 35 ],
-                    [ 47 , 38 ],
-                    [ 48 , 29 ],
-                    [ 49 , 18 ],
-                    [ 50 , 45 ],
-                    [ 51 , 60 ],
-                    [ 52 , 49 ],
-                    [ 53 , 62 ],
-                    [ 54 , 55 ],
-                    [ 55 , 78 ],
-                    [ 56 , 96 ],
-                    [ 57 , 29 ],
-                    [ 58 , 22 ],
-                    [ 59 , 24 ],
-                    [ 60 , 13 ],
-                    [ 61 , 14 ],
-                    [ 62 , 11 ],
-                    [ 63 , 11 ],
-                    [ 64 , 18 ],
-                    [ 65 , 12 ],
-                    [ 66 , 12 ],
-                    [ 67 , 30 ],
-                    [ 68 , 52 ],
-                    [ 69 , 52 ],
-                    [ 70 , 44 ],
-                    [ 71 , 28 ],
-                    [ 72 , 28 ],
-                    [ 73 , 20 ],
-                    [ 74 , 56 ],
-                    [ 75 , 40 ],
-                    [ 76 , 31 ],
-                    [ 77 , 50 ],
-                    [ 78 , 40 ],
-                    [ 79 , 46 ],
-                    [ 80 , 42 ],
-                    [ 81 , 29 ],
-                    [ 82 , 19 ],
-                    [ 83 , 36 ],
-                    [ 84 , 25 ],
-                    [ 85 , 22 ],
-                    [ 86 , 17 ],
-                    [ 87 , 19 ],
-                    [ 88 , 26 ],
-                    [ 89 , 30 ],
-                    [ 90 , 20 ],
-                    [ 91 , 15 ],
-                    [ 92 , 21 ],
-                    [ 93 , 11 ],
-                    [ 94 , 8 ],
-                    [ 95 , 8 ],
-                    [ 96 , 19 ],
-                    [ 97 , 5 ],
-                    [ 98 , 8 ],
-                    [ 99 , 8 ],
-                    [ 100 , 11 ],
-                    [ 101 , 11 ],
-                    [ 102 , 8 ],
-                    [ 103 , 3 ],
-                    [ 104 , 9 ],
-                    [ 105 , 5 ],
-                    [ 106 , 4 ],
-                    [ 107 , 7 ],
-                    [ 108 , 3 ],
-                    [ 109 , 6 ],
-                    [ 110 , 3 ],
-                    [ 111 , 5 ],
-                    [ 112 , 4 ],
-                    [ 113 , 5 ],
-                    [ 114 , 6 ],
-                ]
-#todo to be replaced by json 
-Quraa2 = [
-            ["ar.ahmedajamy",128],
-            ["ar.alafasy",128],
-            ["ar.hudhaify",128],
-            ["ar.husary",128],
-            ["ar.husarymujawwad",128],
-            ["ar.mahermuaiqly",128],
-            ["ar.minshawi",128],
-            ["ar.muhammadayyoub",128],
-            ["ar.muhammadjibreel",128],
-            ["ar.shaatree",128],
-            ["ar.abdulbasitmurattal",192],
-            ["ar.abdullahbasfar",192],
-            ["ar.abdurrahmaansudais",192],
-            ["ar.hanirifai",192],
-            ["ar.abdullahbasfar",32],
-            ["ar.hudhaify",32],
-            ["ar.ibrahimakhbar",32],
-            ["ar.abdulsamad",64],
-            ["ar.aymanswoaid",64],
-            ["ar.minshawimujawwad",64],
-            ["ar.saoodshuraym",64],
-          ] 
- #numbers
-debug = 0 
- #string
- #DataFrame
-df = pd.read_csv('//home/msa/TelegramBot/Quran_repeat_telegram_bot/Quran_aya_index.csv')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
 
-def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
+هذا البرنامج لاختيار أيات متتالية او سور محددة للتلاوة  لمختلف القراء 
+و يقدمها لك كقائمة تشغيل يمكن تشغيلها على جهازك 
+تطلب انترنت لتعمل 
+
+البرنامج قيد التجريب
+ننصح بإستخدام برنامج VLC
+أرسل  /Ayat لاختيار أيات محددة
+أرسل /Sura  لإختيار سور محددة
+
+This program to play certain Ayat or Souar continously
+with diffrent readers 
+we recommend to use VLC player 
+
+send /Ayat for ayat  \nsend /Sura for complete suar
+''')
 
 
-    bot.sendMessage(chat_id, "Command 2 :"+command)
-    #bot.sendMessage(chat_id, "env :"+os.environ.get('Q_ENV'))
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+    text='''
+هذا البرنامج لاختيار أيات متتالية او سور محددة للتلاوة  لمختلف القراء 
+و يقدمها لك كقائمة تشغيل يمكن تشغيلها على جهازك 
+تطلب انترنت لتعمل 
 
+البرنامج قيد التجريب
+ننصح بإستخدام برنامج VLC
+أرسل  /Ayat لاختيار أيات محددة
+أرسل /Sura  لإختيار سور محددة
 
-bot = telepot.Bot(bottoken)
-bot.message_loop(handle)
-print('I am listening...')
+This program to play certain Ayat or Souar continously
+with diffrent readers 
+we recommend to use VLC player 
 
-while (1):
-    try:
-        time.sleep(10)
+send /Ayat for ayat  \nsend /Sura for complete suar
+''')
+
     
-    except KeyboardInterrupt:
-        print('\n Program interrupted')
-        exit()
-  
-    except:
-        print('Other error or exception occured!')
-        exit()
 
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5296769845:AAHSNpG_pPfECfa_wTXuot4FUhERxlfQDlU').build()
+
+
+    # Other handlers
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(unknown_handler)
+
+#last handler
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo) 
+    application.add_handler(echo_handler) 
+        
+    
+    application.run_polling()
